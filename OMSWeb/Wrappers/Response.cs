a32c51689dc971ab25f5fd7e732fbf9d65657aed
Bekzod Allaev <Allaev_@outlook.com>
2021-06-08 23:16:48 +0500

    Response wrappers were added

---
+++ OMSWeb/Wrappers/Response.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OMSWeb.Wrappers
+{
+    /// <summary>
+    /// Base class for wrappers which contains metadata of response
+    /// </summary>
+    /// <typeparam name="T">
+    /// Type of data response
+    /// </typeparam>
+    public class Response<T>
+    {
+        public Response()
+        {
+        }
+
+        public Response(T data)
+        {
+            Succeeded = true;
+            Message = string.Empty;
+            Errors = null;
+            Data = data;
+        }
+
+        public T Data { get; set; }
+        public bool Succeeded { get; set; }
+        public string[] Errors { get; set; }
+        public string Message { get; set; }
+    }
+}
