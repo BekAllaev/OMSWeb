453c61b0aeb0ca1f1eef8cdb027bca4ce6b2fb69
Bekzod Allaev <Allaev_@outlook.com>
2021-06-13 19:31:43 +0500

    Configuration methods were added

--- src/OMSWeb/IoC/ContainerSetup.cs
+++ src/OMSWeb/IoC/ContainerSetup.cs
@@ -1,11 +1,80 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Hangfire;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OMSWeb.Common.DataMock;
+using OMSWeb.Data.Access.DAL;
+using OMSWeb.Queries.Caching.Configuration;
+using OMSWeb.Queries.Caching.Enums;
+using OMSWeb.Queries.Caching.Services;
+using OMSWeb.Queries.Queries;
+using System;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 
 namespace OMSWeb.IoC
 {
     public static class ContainerSetup
     {
+        public static void Setup(IServiceCollection services, IConfiguration configuration)
+        {
+            AddUoW(services, configuration);
+            AddQueryProcessors(services);
+            AddCaching(services, configuration);
+        }
+
+        private static void AddUoW(IServiceCollection services, IConfiguration configuration)
+        {
+            var dataSource = configuration["DataSource"];
+
+            services.AddSingleton<FakeDataGenerator>();
+
+            if (dataSource is "Bogus")
+                services.AddDbContext<NorthwindContext>(options => options.UseInMemoryDatabase("NorthwindContext"));
+            else if (dataSource is "SqlServer")
+                services.AddDbContext<NorthwindContext>(options => options.UseSqlServer(configuration.GetConnectionString("SqlConnection")));
+
+            services.AddScoped<IUnitOfWork>(ctx => new EFUnitOfWork(ctx.GetRequiredService<NorthwindContext>()));
+        }
+
+        private static void AddQueryProcessors(IServiceCollection services)
+        {
+            var exampleProcessorType = typeof(CategoryQueryProcessor);
+            var types = (from t in exampleProcessorType.GetTypeInfo().Assembly.GetTypes()
+                         where t.Namespace == exampleProcessorType.Namespace
+                             && t.GetTypeInfo().IsClass
+                             && t.GetTypeInfo().GetCustomAttribute<CompilerGeneratedAttribute>() == null
+                         select t).ToArray();
+
+            foreach (var type in types)
+            {
+                var interfaceQ = type.GetTypeInfo().GetInterfaces().First();
+                services.AddScoped(interfaceQ, type);
+            }
+        }
+
+        private static void AddCaching(IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<CacheConfiguration>(configuration.GetSection("CacheConfiguration"));
+            services.AddMemoryCache();
+            services.AddTransient<MemoryCacheService>();
+
+            //Register delegate that returns cache service implementation thus no need in service registration
+            //that depends on this cache service
+            services.AddTransient<Func<CacheTech, ICacheService>>(serviceProvider => key =>
+            {
+                switch (key)
+                {
+                    case CacheTech.Memory:
+                        return serviceProvider.GetService<MemoryCacheService>();
+                    default:
+                        return serviceProvider.GetService<MemoryCacheService>();
+                }
+            });
+
+            //services.AddHangfire(x => x.UseSqlServerStorage(Configuration.GetConnectionString("SqlConnection")));
+            services.AddHangfireServer();
+        }
     }
 }
--- src/OMSWeb/Startup.cs
+++ src/OMSWeb/Startup.cs
@@ -1,17 +1,11 @@
 using Hangfire; //NuGet - Hangfire
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
-using OMSWeb.Common.DataMock;
-using OMSWeb.Data.Access.DAL;
-using OMSWeb.Queries.Caching.Configuration;
-using OMSWeb.Queries.Caching.Enums;
-using OMSWeb.Queries.Caching.Services;
-using System;
+using OMSWeb.IoC;
 
 namespace OMSWeb
 {
@@ -27,43 +21,16 @@ namespace OMSWeb
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            var dataSource = Configuration["DataSource"];
+            ContainerSetup.Setup(services, Configuration);
 
             services.AddControllersWithViews().
                 AddNewtonsoftJson(options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
-
-            services.AddSingleton<FakeDataGenerator>();
-
-            if (dataSource is "Bogus")
-                services.AddDbContext<NorthwindContext>(options => options.UseInMemoryDatabase("NorthwindContext"));
-            else if(dataSource is "SqlServer")
-                services.AddDbContext<NorthwindContext>(options => options.UseSqlServer(Configuration.GetConnectionString("SqlConnection")));
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "OMSWeb", Version = "v1" });
             });
-
-            services.Configure<CacheConfiguration>(Configuration.GetSection("CacheConfiguration"));
-            services.AddMemoryCache();
-            services.AddTransient<MemoryCacheService>();
-
-            //Register delegate that returns cache service implementation thus no need in service registration
-            //that depends on this cache service
-            services.AddTransient<Func<CacheTech, ICacheService>>(serviceProvider => key =>
-            {
-                switch (key)
-                {
-                    case CacheTech.Memory:
-                        return serviceProvider.GetService<MemoryCacheService>();
-                    default:
-                        return serviceProvider.GetService<MemoryCacheService>();
-                }
-            });
-
-            //services.AddHangfire(x => x.UseSqlServerStorage(Configuration.GetConnectionString("SqlConnection")));
-            services.AddHangfireServer();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
