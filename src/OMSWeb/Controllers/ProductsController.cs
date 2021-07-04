ecd1f17e714c184fd94dcbc29034aff1d4d0bda0
Bekzod Allaev <Allaev_@outlook.com>
2021-07-04 13:18:24 +0500

    Add Product Controller

---
+++ src/OMSWeb/Controllers/ProductsController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OMSWeb.Data.Access.DAL;
+using OMSWeb.Data.Model;
+using OMSWeb.Dto.Model.ProductDto;
+using OMSWeb.Services.Maps;
+using OMSWeb.Queries.Queries;
+using OMSWeb.Services.Pagination;
+using OMSWeb.Wrappers;
+
+namespace OMSWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductQueryProcessor queryProcessor;
+        private readonly IAutoMapper autoMapper;
+        private readonly IUriService uriService;
+
+        public ProductsController(IProductQueryProcessor queryProcessor,IAutoMapper autoMapper,IUriService uriService)
+        {
+            this.queryProcessor = queryProcessor;
+            this.autoMapper = autoMapper;
+            this.uriService = uriService;
+        }
+
+        // GET: api/Products
+        [HttpGet]
+        public async Task<IActionResult> GetProducts([FromQuery] uint pageNumber, [FromQuery] uint pageSize)
+        {
+            var validPaginationInfo = new PaginationInfo(pageSize, pageNumber);
+            var route = Request.Path.Value;
+
+            var pagedDataQuery = queryProcessor.Get()
+                .Skip(((int)validPaginationInfo.PageNumber - 1) * (int)validPaginationInfo.PageSize)
+                .Take((int)validPaginationInfo.PageSize);
+
+            var pagedData = await pagedDataQuery.ToListAsync();
+
+            var resultCollection = autoMapper.Map<List<DtoProductGet>>(pagedData);
+
+            var totalRecords = await queryProcessor.Get().CountAsync();
+            var pagedReponse = PaginationHelper.CreatePagedReponse<DtoProductGet>(resultCollection, validPaginationInfo, totalRecords, uriService, route);
+
+            return Ok(pagedReponse);
+        }
+
+        // GET: api/Products/5
+        [HttpGet("{id}")]
+        public async Task<DtoProductGet> GetProduct(int id)
+        {
+            var item = await queryProcessor.GetById(id);
+            var model = autoMapper.Map<DtoProductGet>(item);
+            return model;
+        }
+
+        // PUT: api/Products/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<DtoProductGet> PutProduct(int id, [FromBody] DtoProductPut dtoCategory)
+        {
+            var item = await queryProcessor.Update(id, dtoCategory);
+            var model = autoMapper.Map<DtoProductGet>(item);
+            return model;
+        }
+
+        // POST: api/Products
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<DtoProductGet> PostProduct([FromBody] DtoProductPost dtoCategory)
+        {
+            var item = await queryProcessor.Create(dtoCategory);
+
+            var product = autoMapper.Map<DtoProductGet>(item);
+
+            return product;
+        }
+
+        // DELETE: api/Products/5
+        [HttpDelete("{id}")]
+        public async Task DeleteProduct(int id)
+        {
+            await queryProcessor.Delete(id);
+        }
+    }
+}
