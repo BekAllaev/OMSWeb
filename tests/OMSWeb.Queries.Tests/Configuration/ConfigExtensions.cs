18137d6f434a31df19a70e149df8fc93d007427e
Bekzod Allaev <Allaev_@outlook.com>
2021-08-04 21:55:38 +0500

    Add appsettings.test.json

---
+++ tests/OMSWeb.Queries.Tests/Configuration/ConfigExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OMSWeb.Queries.Tests.Configuration
+{
+    public static class ConfigExtensions
+    {
+        public static IConfiguration GetConfiguration()
+        {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.test.json")
+                .Build();
+            return config;
+        }
+    }
+}
