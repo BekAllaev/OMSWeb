d4d2235ffec7ca218ec66b99134bc44dc15ec3e1
Bekzod Allaev <Allaev_@outlook.com>
2021-08-05 20:14:27 +0500

    Use top-level program

--- src/OMSWeb/Program.cs
+++ src/OMSWeb/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using OMSWeb;
 using OMSWeb.Common.DataMock;
 using OMSWeb.Data.Access.DAL;
 using System;
@@ -10,50 +11,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace OMSWeb
+//https://exceptionnotfound.net/ef-core-inmemory-asp-net-core-store-database/
+var host = CreateHostBuilder(args).Build();
+bool isDataFake;
+
+var config = host.Services.GetRequiredService<IConfiguration>();
+
+var dataSource = config["DataSource"];
+
+if (dataSource is "SqlServer")
+    isDataFake = false;
+else
+    isDataFake = true;
+
+if (isDataFake)
 {
-    public class Program
+    //1. Find the service layer within our scope.
+    using (var scope = host.Services.CreateScope())
     {
-        //https://exceptionnotfound.net/ef-core-inmemory-asp-net-core-store-database/
-        public static void Main(string[] args)
-        {
-            var host = CreateHostBuilder(args).Build();
-            bool isDataFake;
+        //2. Get the instance of BoardGamesDBContext in our services layer
+        var services = scope.ServiceProvider;
 
-            var config = host.Services.GetRequiredService<IConfiguration>();
+        var context = services.GetRequiredService<NorthwindContext>();
+        var generator = services.GetRequiredService<FakeDataGenerator>();
 
-            var dataSource = config["DataSource"];
-
-            if (dataSource is "SqlServer")
-                isDataFake = false;
-            else
-                isDataFake = true;
-
-            if (isDataFake)
-            {
-                //1. Find the service layer within our scope.
-                using (var scope = host.Services.CreateScope())
-                {
-                    //2. Get the instance of BoardGamesDBContext in our services layer
-                    var services = scope.ServiceProvider;
-
-                    var context = services.GetRequiredService<NorthwindContext>();
-                    var generator = services.GetRequiredService<FakeDataGenerator>();
-
-                    //3. Call the DataGenerator to create sample data
-                    FakeDataSeeder.Initialize(context, generator);
-                }
-            }
-
-            //Continue to run the application
-            host.Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        //3. Call the DataGenerator to create sample data
+        FakeDataSeeder.Initialize(context, generator);
     }
 }
+
+//Continue to run the application
+host.Run();
+
+static IHostBuilder CreateHostBuilder(string[] args) =>
+    Host.CreateDefaultBuilder(args)
+        .ConfigureWebHostDefaults(webBuilder =>
+        {
+            webBuilder.UseStartup<Startup>();
+        });
