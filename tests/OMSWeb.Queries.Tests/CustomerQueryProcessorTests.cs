2fb1c0e6efe4f5935f05900890837a9fb70f58e3
Bekzod Allaev <Allaev_@outlook.com>
2021-08-05 19:55:08 +0500

    Add tests for CustomerQueryProcessor

--- tests/OMSWeb.Queries.Tests/CustomerQueryProcessorTests.cs
+++ tests/OMSWeb.Queries.Tests/CustomerQueryProcessorTests.cs
@@ -1,10 +1,12 @@
-﻿using Hangfire;
+﻿using FluentAssertions;
+using Hangfire;
 using Hangfire.SqlServer;
 using Microsoft.Extensions.Configuration;
 using MockQueryable.Moq;
 using Moq;
 using OMSWeb.Data.Access.DAL;
 using OMSWeb.Data.Model;
+using OMSWeb.Dto.Model.CutomerDto;
 using OMSWeb.Queries.Caching.Enums;
 using OMSWeb.Queries.Caching.Services;
 using OMSWeb.Queries.Interfaces;
@@ -15,6 +17,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace OMSWeb.Queries.Tests
 {
@@ -49,6 +52,94 @@ namespace OMSWeb.Queries.Tests
             JobStorage.Current = new SqlServerStorage(ConfigExtensions.GetConfiguration().GetConnectionString("SqlConnection"));
 
             customerQueryProcessor = new CustomerQueryProcessor(unitOfWork.Object, func);
+        }
+
+        [Fact]
+        public void GetShouldReturnAll()
+        {
+            customers.Add(new Customer() { CustomerID = "ABC", CompanyName = "Customer1" });
+
+            var result = customerQueryProcessor.Get().ToList();
+            result.Count.Should().Be(1);
+        }
+
+        [Fact]
+        public async Task GetByIdShouldReturnCorrectInstance()
+        {
+            customers.Add(new Customer() { CustomerID = "ABC", CompanyName = "Customer1" });
+            customers.Add(new Customer() { CustomerID = "ABC", CompanyName = "Customer2" });
+
+            var id = customers[0].CustomerID;
+
+            var result = await customerQueryProcessor.GetById(id);
+            result.CustomerID.Should().Be(id);
+        }
+
+        [Fact]
+        public void GetByIdShouldThrowExceptionIfNotFound()
+        {
+            Func<Task> func = async () => await customerQueryProcessor.GetById("ABC");
+
+            func.Should().Throw<KeyNotFoundException>();
+        }
+
+        [Fact]
+        public async Task DeleteShouldRemoveItem()
+        {
+            string id = "ABC";
+
+            customers.Add(new Customer() { CustomerID = id });
+
+            await customerQueryProcessor.Delete(id);
+
+            customers.Should().HaveCount(0);
+
+            unitOfWork.Verify(x => x.Commit());
+        }
+
+        [Fact]
+        public void DeleteShouldThrowExceptionIfNotFound()
+        {
+            Func<Task> func = async () => await customerQueryProcessor.Delete("ABC");
+
+            func.Should().Throw<KeyNotFoundException>();
+        }
+
+        [Fact]
+        public async Task CreateShouldSaveNew()
+        {
+            var customer = await customerQueryProcessor.Create(new DtoCustomerPutPost()
+            {
+                CompanyName = "Supplier1"
+            });
+
+            customers.Should().Contain(customer);
+
+            unitOfWork.Verify(x => x.CommitAsync());
+        }
+
+        [Fact]
+        public async Task UpdateShouldEditPropertyValue()
+        {
+            string id = "ABC";
+
+            customers.Add(new Customer() { CustomerID = id, CompanyName = "Supplier1", Phone = "123-45-67" });
+
+            string newPhone = "123-45-67-89";
+
+            var customer = await customerQueryProcessor.Update(id, new DtoCustomerPutPost() { Phone = newPhone });
+
+            customer.Phone.Should().Be(newPhone);
+
+            unitOfWork.Verify(x => x.Commit());
+        }
+
+        [Fact]
+        public void UpdateShouldThrowExceptionIfNotFound()
+        {
+            Func<Task> func = async () => await customerQueryProcessor.Update("ABC", new DtoCustomerPutPost());
+
+            func.Should().Throw<KeyNotFoundException>();
         }
     }
 }
