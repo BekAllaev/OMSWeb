872d8f73477e0fc89d20199456d9e0f338122442
Bekzod Allaev <Allaev_@outlook.com>
2021-08-07 11:55:12 +0500

    Mock dependencies

--- tests/OMSWeb.Queries.Tests/OrderQueryProcessorTests.cs
+++ tests/OMSWeb.Queries.Tests/OrderQueryProcessorTests.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿using Hangfire;
+using Hangfire.SqlServer;
+using Microsoft.Extensions.Configuration;
+using MockQueryable.Moq;
+using Moq;
+using OMSWeb.Data.Access.DAL;
+using OMSWeb.Data.Model;
+using OMSWeb.Queries.Caching.Enums;
+using OMSWeb.Queries.Caching.Services;
+using OMSWeb.Queries.Interfaces;
+using OMSWeb.Queries.Queries;
+using OMSWeb.Queries.Tests.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +20,31 @@ namespace OMSWeb.Queries.Tests
 {
     public class OrderQueryProcessorTests
     {
+        private readonly Mock<IUnitOfWork> unitOfWork;
+
+        private readonly List<Order> orders;
+        private readonly IOrderQueryProcessor orderQueryProcessor;
+
         public OrderQueryProcessorTests()
         {
+            ICacheService func(CacheTech cacheTech) => new Mock<ICacheService>().Object;
 
+            unitOfWork = new();
+            orders = new();
+
+            //Look this link to see what BuildMock method do
+            //https://github.com/romantitov/MockQueryable#how-do-i-get-started
+            var mock = orders.AsQueryable().BuildMock();
+
+            unitOfWork.Setup(x => x.Query<Order>()).Returns(mock.Object);
+            unitOfWork.Setup(x => x.Delete(It.IsAny<Order>())).Callback<Order>(order => orders.Remove(order));
+            unitOfWork.Setup(x => x.Add(It.IsAny<Order>())).Callback<Order>(order => orders.Add(order));
+
+            JobStorage.Current = new SqlServerStorage(ConfigExtensions.GetConfiguration().GetConnectionString("SqlConnection"));
+
+            orderQueryProcessor = new OrderQueryProcessor(unitOfWork.Object, func);
         }
+
+
     }
 }
