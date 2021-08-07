c727dced33c5cbb4a50c3e07a6d41b84f798483d
Bekzod Allaev <Allaev_@outlook.com>
2021-08-07 11:17:47 +0500

    Mock dependencies

--- tests/OMSWeb.Queries.Tests/CategoryQueryProcessorTests.cs
+++ tests/OMSWeb.Queries.Tests/CategoryQueryProcessorTests.cs
@@ -1,12 +1,18 @@
-﻿using Moq;
+﻿using Hangfire;
+using Hangfire.SqlServer;
+using Microsoft.Extensions.Configuration;
+using MockQueryable.Moq;
+using Moq;
 using OMSWeb.Data.Access.DAL;
 using OMSWeb.Data.Model;
+using OMSWeb.Queries.Caching.Enums;
+using OMSWeb.Queries.Caching.Services;
 using OMSWeb.Queries.Interfaces;
+using OMSWeb.Queries.Queries;
+using OMSWeb.Queries.Tests.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OMSWeb.Queries.Tests
 {
@@ -19,7 +25,28 @@ namespace OMSWeb.Queries.Tests
 
         public CategoryQueryProcessorTests()
         {
+            Func<CacheTech, ICacheService> func = cacheTech => new Mock<ICacheService>().Object;
 
+            unitOfWork = new();
+            categories = new();
+
+            //Look this link to see what BuildMock method do
+            //https://github.com/romantitov/MockQueryable#how-do-i-get-started
+            var mock = categories.AsQueryable().BuildMock();
+
+            unitOfWork.Setup(x => x.Query<Category>()).Returns(mock.Object);
+            unitOfWork.Setup(x => x.Delete(It.IsAny<Category>())).Callback<Category>(category => categories.Remove(category));
+            unitOfWork.Setup(x => x.Add(It.IsAny<Category>())).Callback<Category>(category => categories.Add(category));
+            unitOfWork.Setup(x => x.Update(It.IsAny<Category>())).Callback<Category>(category =>
+            {
+                var item = categories.Find(x => x.CategoryID == category.CategoryID);
+
+                item = category;
+            });
+
+            JobStorage.Current = new SqlServerStorage(ConfigExtensions.GetConfiguration().GetConnectionString("SqlConnection"));
+
+            categoryQueryProcessor = new CategoryQueryProcessor(unitOfWork.Object, func);
         }
     }
 }
