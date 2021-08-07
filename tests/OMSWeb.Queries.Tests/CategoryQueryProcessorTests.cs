5f49d68ec8abc917e54a753ffa91451db053c4ad
Bekzod Allaev <Allaev_@outlook.com>
2021-08-07 11:11:13 +0500

    Add CategoryQueryProcessorTests

---
+++ tests/OMSWeb.Queries.Tests/CategoryQueryProcessorTests.cs
@@ -0,0 +1,25 @@
+﻿using Moq;
+using OMSWeb.Data.Access.DAL;
+using OMSWeb.Data.Model;
+using OMSWeb.Queries.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OMSWeb.Queries.Tests
+{
+    public class CategoryQueryProcessorTests
+    {
+        private readonly Mock<IUnitOfWork> unitOfWork;
+
+        private readonly List<Category> categories;
+        private readonly ICategoryQueryProcessor categoryQueryProcessor;
+
+        public CategoryQueryProcessorTests()
+        {
+
+        }
+    }
+}
